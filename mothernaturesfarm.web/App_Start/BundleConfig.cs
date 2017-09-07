d69d50f33b62195ee8c18d776aca5c4d19907d40
Rob DeMartino <rdemartino@gmail.com>
2017-09-07 15:00:30 -0700

    re-organized css/js files, cleaned up code/html.

--- mothernaturesfarm.web/App_Start/BundleConfig.cs
+++ mothernaturesfarm.web/App_Start/BundleConfig.cs
@@ -8,15 +8,25 @@ namespace mothernaturesfarm.web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Content/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new StyleBundle("~/styles/default").Include(
+                "~/Content/Styles/foundation.css",
+                "~/Content/Styles/style.css"));
+
+            bundles.Add(new ScriptBundle("~/scripts/jquery").Include(
+                "~/Content/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts/common").Include(
+                "~/Content/Scripts/what-input.js",
+                "~/Content/Scripts/foundation.js",
+                "~/Content/Scripts/app.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts/jqueryval").Include(
                         "~/Content/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("~/scripts/modernizr").Include(
                         "~/Content/Scripts/modernizr-*"));
            
         }
