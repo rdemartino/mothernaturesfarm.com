e3c13b94c12bffcaa7486e2c9e772ab130474985
rdemartino <rdemartino@gmail.com>
2020-02-07 12:38:31 -0700

    Added resource strings to support date changes and item costs

---
+++ mothernaturesfarm.web/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mothernaturesfarm.web.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToString(this DateTime dateTime, string format, bool useExtendedSpecifiers)
+        {
+            return useExtendedSpecifiers
+                ? dateTime.ToString(format)
+                    .Replace("nn", dateTime.Day.ToOccurrenceSuffix().ToLower())
+                    .Replace("NN", dateTime.Day.ToOccurrenceSuffix().ToUpper())
+                : dateTime.ToString(format);
+        }
+    }
+}
---
+++ mothernaturesfarm.web/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mothernaturesfarm.web.Models
+{
+    public static class Class1
+    {
+
+    }
+}
